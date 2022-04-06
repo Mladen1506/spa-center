31669a796feeae0bafef4989e0bf5f6c3ee35059
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-04-06 14:39:32 -0400

    css basics

---
+++ spa-react/src/components/Layout.jsx
@@ -0,0 +1,28 @@
+const Layout = (props)=>{
+  return (
+    <div className="wrapper">
+      <header>
+        <div className="logo">LOGO</div>
+        <nav>
+          <div className="option">Nesto</div>
+          <div className="option">Nes</div>
+          <div className="option">Nestooooo</div>
+          <div className="option">Neto</div>
+          <div className="option">Nestooo</div>
+          <div className="option">Nesto</div>
+        </nav>
+      </header>
+      <div className="page-body">
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </div>
+      <footer>
+    footer
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
