a43caef8bc33d1d1c48116bb271822f53c71a118
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-04-06 15:40:40 -0400

    responsive menu, nav and drawer

--- spa-react/src/components/Layout.jsx
+++ spa-react/src/components/Layout.jsx
@@ -1,8 +1,24 @@
-const Layout = (props)=>{
+import { useState } from "react";
+
+const Layout = (props) => {
+  const [drawerOpened, setDrawerOpened] = useState(false);
+
+  const handleClickHamburger = ()=>{
+    if (drawerOpened) {
+      setDrawerOpened(false);
+    } else {
+      setDrawerOpened(true);
+    }
+  };
+
   return (
     <div className="wrapper">
       <header>
-        <div className="logo">LOGO</div>
+        <div className="logo">SPA LOGO</div>
+        <div className="flex-1"></div>
+        <span className="hamburger" onClick={handleClickHamburger}>
+          <i className="fa fa-bars" aria-hidden="true"></i>
+        </span>
         <nav>
           <div className="option">Nesto</div>
           <div className="option">Nes</div>
@@ -11,7 +27,18 @@ const Layout = (props)=>{
           <div className="option">Nestooo</div>
           <div className="option">Nesto</div>
         </nav>
+        <div className={drawerOpened ? "fix-drawer-push opened": "fix-drawer-push"}></div>
       </header>
+
+      <nav className={drawerOpened ? "drawer opened" : "drawer"}>
+        <div className="option">Nesto</div>
+        <div className="option">Nes</div>
+        <div className="option">Nestooooo</div>
+        <div className="option">Neto</div>
+        <div className="option">Nestooo</div>
+        <div className="option">Nesto</div>
+      </nav>
+
       <div className="page-body">
         <section></section>
         <section></section>
@@ -19,7 +46,7 @@ const Layout = (props)=>{
         <section></section>
       </div>
       <footer>
-    footer
+        footer
       </footer>
     </div>
   );
