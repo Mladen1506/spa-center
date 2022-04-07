515a386f66110a1c42d8a99468147fd979ec12e1
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-04-07 16:10:54 -0400

    bootstrap grid

--- spa-react/src/components/Layout.jsx
+++ spa-react/src/components/Layout.jsx
@@ -60,13 +60,34 @@ const Layout = (props) => {
         <section>
           <div className="inner">
             <div className="grid-container">
-              <div className="grid-item"><img src="/static/img/lejla.jpeg" /></div>
-              <div className="grid-item"><img src="/static/img/lejla.jpeg" /></div>
-              <div className="grid-item"><img src="/static/img/lejla.jpeg" /></div>
-              <div className="grid-item"><img src="/static/img/lejla.jpeg" /></div>
-
+              <div className="grid-item"><img className="grid-img" src="/static/img/lejla.jpeg" /></div>
+              <div className="grid-item"><img className="grid-img" src="/static/img/lejla.jpeg" /></div>
+              <div className="grid-item"><img className="grid-img" src="/static/img/lejla.jpeg" /></div>
+              <div className="grid-item"><img className="grid-img" src="/static/img/lejla.jpeg" /></div>
             </div>
+
+            <div className="container222">
+              <div className="row">
+                <div className="col-12 col-sm-3 col-md-4 col-lg-3"><img className="grid-img" src="/static/img/lejla.jpeg" /></div>
+                <div className="col-12 col-sm-3 col-md-4 col-lg-3"><img className="grid-img" src="/static/img/lejla.jpeg" /></div>
+                <div className="col-12 col-sm-3 col-md-4 col-lg-3"><img className="grid-img" src="/static/img/lejla.jpeg" /></div>
+                <div className="col-12 col-sm-3 col-md-4 col-lg-3"><img className="grid-img" src="/static/img/lejla.jpeg" /></div>
+              </div>
+            </div>
+
             <Btn />
+
+            <div className="container222">
+              <div className="row">
+                <div className="col-12 col-md-6">
+                  <h1>Creative excellence and unparalleled hospitality are the driving forces behind the Muse experience.</h1>
+                </div>
+                <div className="col-12 col-sm-6">
+                  <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+                </div>
+              </div>
+            </div>
+
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
         </section>
