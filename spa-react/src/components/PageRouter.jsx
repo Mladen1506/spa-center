cfab18e40dd2c8f3871a19ce51600ac9e6252893
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-04-09 16:08:47 -0400

    universal  img component almost done

--- spa-react/src/components/PageRouter.jsx
+++ spa-react/src/components/PageRouter.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import PageFacials from "./pages/PageFacials";
 import PageSpaSkinCare from "./pages/PageSpaSkinCare";
 import PageBasicBotanical from "./pages/PageBasicBotanical.jsx";
+import PageMassageBody from "./pages/PageMassageBody";
 
 
 const PageRouter = (props) => {
@@ -18,27 +19,31 @@ const PageRouter = (props) => {
     );
   } else if (route === 'SPA_SKIN_CARE') {
     jsx = (
-      <PageSpaSkinCare/>
+      <PageSpaSkinCare />
     );
   } else if (route === 'FACIALS') {
     jsx = (
-      <PageFacials/>
-      );
-    } else if (route === 'BASIC_BOTANICAL') {
-      jsx = (
-        <PageBasicBotanical />
-        );
-        /*
-      } else if (route === 'MEDI_SKIN_CARE') {
-        jsx = (
-          <PageSpaSkinCare/>
-        );
-        */
-      } else if (route === 'AQUA_GOLD') {
-        jsx = (
-          <PageAquaGold />
-          );
-        } else {
+      <PageFacials />
+    );
+  } else if (route === 'BASIC_BOTANICAL') {
+    jsx = (
+      <PageBasicBotanical />
+    );
+    /*
+  } else if (route === 'MEDI_SKIN_CARE') {
+    jsx = (
+      <PageSpaSkinCare/>
+    );
+    */
+  } else if (route === 'AQUA_GOLD') {
+    jsx = (
+      <PageAquaGold />
+    );
+  } else if (route === 'MASSAGE_BODY') {
+    jsx = (
+      <PageMassageBody/>
+    );
+  } else {
     jsx = (
       <div>Page {route} not found!</div>
     );
---
+++ spa-react/src/components/pages/PageMassageBody.jsx
@@ -0,0 +1,30 @@
+import Image from "../image";
+import Page from "../Page";
+import PageContent from "../PageContent";
+
+const PageMassageBody = (props) => {
+  return (
+    <Page>
+      <PageContent>
+        <div className="fl-module-content fl-node-content">
+          <div className="fl-rich-text">
+            <h3>Massage &amp; Body</h3>
+            <Image left src={"/static/img/pages/Massage_woman_trifolf_cover-150x150.jpeg"} alt="Neki alt" caption="Neki caption" />
+            <p>Azure Dream Spa wants you to walk through our door and leave your cares behind. Let us pamper you with a variety of body spa treatments designed to relax, detox, and revive.</p>
+            <h6>Massage</h6>
+            <Image right src={"/static/img/pages/Massage_woman_trifolf_cover-150x150.jpeg"} alt="Neki alt" />
+            <p>Experience the healing hands of our nationally-certified massage therapists, who are trained in a variety of therapies -- from hot stone to stress-relief to deep tissue. <a href="https://azuredreamspa.com/massage-body/massage-2/">See more...</a></p>
+            <h6>Organic Hydrating Body Spa</h6>
+            <p>A gentle yet thorough exfoliation of the entire body, using organic sugar crystals, followed by a light massage with organic body oil and lotion to moisturize your skin, this aromatic treatment leaves your body smooth and your soul relaxed. <a href="https://azuredreamspa.com/massage-body/organic-hydrating-body-spa/">See more...</a></p>
+            <h6>Cellulite Body Wrap</h6>
+            <p>Cocoon yourself in our body wrap, for softer, firmer, silkier skin, and enjoy the benefits of cellulite reduction. Let the relaxation begin! <a href="https://azuredreamspa.com/massage-body/cellulite-body-wrap/">See more...</a></p>
+            <h6>Organic Back Skincare Treatment</h6>
+            <p>The only way to improve on the enjoyment of a back massage is to add a "back facial." With this two-in-one treatment, you'll enjoy all the relaxation of a back massage, combined with the cleansing, moisturizing, and soothing effects of an organic facial. Look and feel beautiful! <a href="https://azuredreamspa.com/massage-body/organic-back-skincare/">See more...</a></p>
+          </div>
+        </div>
+      </PageContent>
+    </Page>
+  );
+};
+
+export default PageMassageBody;
