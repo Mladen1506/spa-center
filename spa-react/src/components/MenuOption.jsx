03c67c8d92d33336cf61361cc5a8c20425bd0ec0
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-04-09 13:37:34 -0400

    solved submenu options, solved click propagation

--- spa-react/src/components/MenuOption.jsx
+++ spa-react/src/components/MenuOption.jsx
@@ -1,10 +1,38 @@
 import { useSelector } from "react-redux";
+import { get_children_routes } from "../utils/routes-lib";
 
 const MenuOption = (props) => {
-  // const route = useSelector(state => state.route);
+  const route = useSelector(state => state.route);
+  const item = props.item;
+
+  // props.level je level ove opcije u meniju
+  const childrenLevel = item.level + 1;
+
+  const childrenOptions = get_children_routes(item.route, childrenLevel);
+  // console.log(childrenOptions);
+
+  const _handleClickOption = props._handleClickOption;
+
+
   return (
-    <div className={props.active ? "option active" : "option"} onClick={props.handleClick}>{props.title}
-    <div className="submenu">SUB</div>
+    <div
+      className={route === item.route ? "option active" : "option"}
+      onClick={(e) => { e.stopPropagation(); _handleClickOption(item.route) }}
+    >{item.title}
+      <div className="submenu">
+
+        {childrenOptions.map((item, index) => {
+          return (
+            <MenuOption
+              key={index}
+              level={1}
+              item={item}
+              _handleClickOption={_handleClickOption}
+            />
+          );
+        })}
+
+      </div>
     </div>
   );
 };
