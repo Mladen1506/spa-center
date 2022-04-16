a5cafb8e1479b86e02596afbfd9993e089707540
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-04-16 16:16:40 -0400

    added url for google maps

--- spa-react/src/components/special-pages/PageContact.jsx
+++ spa-react/src/components/special-pages/PageContact.jsx
@@ -18,6 +18,9 @@ const PageContact = (props) => {
   // "https://www.google.com/maps/search/?api=1&query=" + ll[0] + ',' + ll[1];
   const googleMapsUrl = "https://www.google.com/maps/search/?api=1&query=" + 38.8810 + ',' + -77.1074;
   // <a target="_blank" href={googleMapsUrl}>Open on Google Maps</a>
+  
+  // https://www.google.com/maps/dir/?api=1&destination=38.881,-77.1074
+  const googleMapsDirectionsUrl = "https://www.google.com/maps/dir/?api=1&destination=" + 38.8810 + ',' + -77.1074;
 
 
   return (
@@ -39,6 +42,7 @@ const PageContact = (props) => {
           <div className="col-12 col-lg-6">
             <UniversalMap ll={coordinatesOfSpaArlingtonQuincy} zoom={13} />
           <a className="link-open-map" target="_blank" rel="noreferrer" href={googleMapsUrl}>Open on Google Maps</a>
+          <a className="link-open-map" target="_blank" rel="noreferrer" href={googleMapsDirectionsUrl}>Show directions on Google Maps</a>
           </div>
           <div className="col-12 col-lg-6">
             <ContactForm />
