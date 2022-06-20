46f2128ba13b7a8d5c9e2f6d934ac243dacd6147
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-06-20 14:58:54 -0400

    updates per request

--- spa-react/src/components/pages/PageBrowLamination.jsx
+++ spa-react/src/components/pages/PageBrowLamination.jsx
@@ -5,7 +5,9 @@ const PageBrowLamination = () => {
   return (
     <Page>
       <PageContent>
-        <div>Comming Soon...</div>
+        <h6>Brow Lamination</h6>
+        <p>Brow lamination is almost like a perm for you eyebrows which should last 5-8 weeks. A ways of enhancing what you already have. The treatment is often paired with a brow tint for a bolder enhanced look.</p>
+        <p>Brow Lamination $130</p>
       </PageContent>
     </Page>
   );
