d4b21e80cc06a789e43a800804d7e2517cbe5ff9
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-07-14 06:30:54 -0400

    update per request

--- spa-react/src/components/pages/PageHydrafacialMembership.jsx
+++ spa-react/src/components/pages/PageHydrafacialMembership.jsx
@@ -27,31 +27,48 @@ const PageHydrafacialMembership = (props) => {
               <tr>
                 <td>30 Min</td>
                 <td>1</td>
-                <td>$199</td>
+                <td>$200</td>
               </tr>
-              {/* <tr>
-                <td>30 Min</td>
-                <td>4</td>
-                <td>$716</td>
-              </tr> */}
               <tr>
                 <td>30 Min</td>
                 <td>6</td>
-                <td>$1015</td>
+                <td>$1050</td>
               </tr>
-              {/* <tr>
-                <td>30 Min</td>
-                <td>12</td>
-                <td>$1795</td>
-              </tr> */}
             </tbody>
           </table>
 
 
           <h6>DELUXE HYDRAFACIAL</h6>
           <p>These invigorating treatments include all of the essentials of the Signature HydraFacial while addressing specific skin concerns through HydraFacial’s boosters and protocols.</p>
-          <h6>Restorative</h6>
-          <p>Add CTGF™ to improve the appearance of skin tone, texture, and elasticity.</p>
+
+          <table className="table table-bordered">
+            <tbody>
+              <tr>
+                <td>
+                  <p><b>Minutes</b></p>
+                </td>
+                <td>
+                  <p><b>Packages</b></p>
+                </td>
+                <td>
+                  <p><b>Price</b></p>
+                </td>
+              </tr>
+              <tr>
+                <td>45 Min</td>
+                <td>1</td>
+                <td>$285</td>
+              </tr>
+              <tr>
+                <td>45 Min</td>
+                <td>6</td>
+                <td>$1560</td>
+              </tr>
+            </tbody>
+          </table><br></br>
+
+          {/* <h6>Restorative</h6> */}
+          {/* <p>Add CTGF™ to improve the appearance of skin tone, texture, and elasticity.</p>
 
           <table className="table table-bordered">
             <tbody>
@@ -71,25 +88,16 @@ const PageHydrafacialMembership = (props) => {
                 <td>1</td>
                 <td>$295</td>
               </tr>
-              {/* <tr>
-                <td>45 Min</td>
-                <td>4</td>
-                <td>$1120</td>
-              </tr> */}
               <tr>
                 <td>45 Min</td>
                 <td>6</td>
                 <td>$1595</td>
               </tr>
-              {/* <tr>
-                <td>45 Min</td>
-                <td>12</td>
-                <td>$2830</td>
-              </tr> */}
             </tbody>
-          </table><br></br>
+          </table><br></br> */}
 
-          <h6>Age-Refinement</h6>
+          
+          {/* <h6>Age-Refinement</h6>
           <p>Add a DermaBuilder™ boost to reduce the appearance of fine lines and wrinkles.</p>
           <table className="table table-bordered">
             <tbody>
@@ -109,25 +117,16 @@ const PageHydrafacialMembership = (props) => {
                 <td>1</td>
                 <td>$250</td>
               </tr>
-              {/* <tr>
-                <td>45 Min</td>
-                <td>4</td>
-                <td>$950</td>
-              </tr> */}
               <tr>
                 <td>45 Min</td>
                 <td>6</td>
                 <td>$1350</td>
               </tr>
-              {/* <tr>
-                <td>45 Min</td>
-                <td>12</td>
-                <td>$2400</td>
-              </tr> */}
             </tbody>
-          </table><br></br>
+          </table><br></br> */}
+          
 
-          <h6>Radiance</h6>
+          {/* <h6>Radiance</h6>
           <p>Add a Britenol® Boost to minimize the appearance of dark spots.</p>
           <table className="table table-bordered">
             <tbody>
@@ -147,25 +146,15 @@ const PageHydrafacialMembership = (props) => {
                 <td>1</td>
                 <td>$250</td>
               </tr>
-              {/* <tr>
-                <td>45 Min</td>
-                <td>4</td>
-                <td>$950</td>
-              </tr> */}
               <tr>
                 <td>45 Min</td>
                 <td>6</td>
                 <td>$1350</td>
               </tr>
-              {/* <tr>
-                <td>45 Min</td>
-                <td>12</td>
-                <td>$2400</td>
-              </tr> */}
             </tbody>
-          </table><br></br>
+          </table><br></br> */}
 
-          <h6>Clarifying</h6>
+          {/* <h6>Clarifying</h6>
           <p>Extended Extractions and add Blue LED Light Therapy to remove congestion.</p>
           <table className="table table-bordered">
             <tbody>
@@ -185,25 +174,15 @@ const PageHydrafacialMembership = (props) => {
                 <td>1</td>
                 <td>$225</td>
               </tr>
-              {/* <tr>
-                <td>45 Min</td>
-                <td>4</td>
-                <td>$855</td>
-              </tr> */}
               <tr>
                 <td>45 Min</td>
                 <td>6</td>
                 <td>$1215</td>
               </tr>
-              {/* <tr>
-                <td>45 Min</td>
-                <td>12</td>
-                <td>$2160</td>
-              </tr> */}
             </tbody>
-          </table><br></br>
+          </table><br></br> */}
 
-          <h6>60-MIN. PLATINUM HYDRAFACIAL</h6>
+          <h6>PLATINUM HYDRAFACIAL</h6>
           <p>The ultimate HydraFacial experience. Begin the detoxification process with Lymphatic Drainage. Follow with the Signature HydraFacial to deeply cleanse, extract and hydrate the skin. Address specific skin concerns with a booster of your choice. This treatment concludes with LED Light Therapy to further reduce the visible signs of aging.</p>
 
           <table className="table table-bordered">
@@ -224,21 +203,39 @@ const PageHydrafacialMembership = (props) => {
                 <td>1</td>
                 <td>$325</td>
               </tr>
-              {/* <tr>
-                <td>60 Min</td>
-                <td>4</td>
-                <td>$1235</td>
-              </tr> */}
               <tr>
                 <td>60 Min</td>
                 <td>6</td>
-                <td>$1755</td>
+                <td>$1800</td>
               </tr>
-              {/* <tr>
-                <td>60 Min</td>
-                <td>12</td>
-                <td>$3120</td>
-              </tr> */}
+            </tbody>
+          </table>
+
+          <h6>PLATINUM ULTIMATE</h6>
+          <p><b>It is our platinum for those that wish not to miss facial massages</b></p>
+          <table className="table table-bordered">
+            <tbody>
+              <tr>
+                <td>
+                  <p><b>Minutes</b></p>
+                </td>
+                <td>
+                  <p><b>Packages</b></p>
+                </td>
+                <td>
+                  <p><b>Price</b></p>
+                </td>
+              </tr>
+              <tr>
+                <td>75 Min</td>
+                <td>1</td>
+                <td>$385</td>
+              </tr>
+              <tr>
+                <td>75 Min</td>
+                <td>6</td>
+                <td>$2160</td>
+              </tr>
             </tbody>
           </table>
 
